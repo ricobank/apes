6afff381e23eb3914f014ac78158e2293171314e
dmfxyz <d@dmf.xyz>
2022-05-08 12:02:15 -0700

    unit test missing lvalue

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 const it = require('tapzero').test
+const parseError = new RegExp('^could not parse$')
 
 const { show, parse } = require('./apes.js')
 
-it('parse', t=>{
+it('parseBasicValid', t=>{
     t.ok(parse('x = weth.balance()'))
+})
+
+// can't parse if no lvalue provided
+it('parseInvalidNullLvalue', t=> {
+    t.throws( _ => {parse('=weth.balance()')}, parseError)
 })
